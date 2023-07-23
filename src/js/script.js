ac73eb007dbc92e206ff67529a2b8d8e1746a702
{morimasashi} <{m.m.19910905@gmail.com}>
2023-07-24 07:49:15 +0900

    afterTextFB_230724

--- src/js/script.js
+++ src/js/script.js
@@ -9,16 +9,15 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
         $(".js-drawer").fadeToggle();
     });
 
-
 // スライダー
 // js-top-mv-sp-swiper
     let topMvSpSwipeOption = {
         loop: true,
         effect: 'fade',
-        autoplay: {
-            delay: 3000,
-            disableOnInteraction: false,
-        },
+        // autoplay: {
+        //     delay: 3000,
+        //     disableOnInteraction: false,
+        // },
         speed: 1200,
     }
     const swiper = new Swiper(".js-top-mv__sp-swiper", topMvSpSwipeOption)
@@ -27,10 +26,10 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
     let topMvPcSwipeOption = {
         loop: true,
         effect: 'fade',
-        autoplay: {
-            delay: 4000,
-            disableOnInteraction: false,
-        },
+        // autoplay: {
+        //     delay: 4000,
+        //     disableOnInteraction: false,
+        // },
         speed: 1500,
     }
     new Swiper(".js-top-mv__pc-swiper", topMvPcSwipeOption)
@@ -38,26 +37,16 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
 // js-top-campaign__swiper
     let topCampaignSwipeOption = {
         loop: true,
-        autoplay: {
-            delay: 4000,
-            disableOnInteraction: false,
-        },
+        // autoplay: {
+        //     delay: 4000,
+        //     disableOnInteraction: false,
+        // },
         speed: 700,
-        slidesPerView: 1.26,
+        slidesPerView: 1.2,
         spaceBetween: 24,
         breakpoints: {
-            600: { //600px以上
-                slidesPerView: 2,
-                centeredSlides: true,
-            },
-            768: { //768px以上
-                slidesPerView: 2.5,
-                centeredSlides: true,
-            },
-            1440: { //1440px以上
-                slidesPerView: 3.955,
-                centeredSlides: true,
-                spaceBetween: 40,
+            1024: { // 1024px以上
+                slidesPerView: 3.5,
             }
         },
         navigation: {
@@ -110,69 +99,65 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
         $('html,body').animate({ scrollTop: targetY }, time, 'swing');
         return false;
     });
+
 });
 
 
-
-    document.addEventListener('DOMContentLoaded',function(){
-        const mm = gsap.matchMedia();
-
+document.addEventListener('DOMContentLoaded',function(){
+    const mm = gsap.matchMedia();
     // opening
-        let openingTL = gsap.timeline();
-        let turtleImages = document.querySelectorAll('.js-top-mv__animation-image > *');
-        let mvTitles = document.querySelectorAll('.js-top-mv__title > *');
-        // 767px以下の時
-        mm.add('(max-width:767px)',function(){
-            openingTL
-            .to('.js-header',{delay:.5,y:0, duration:1})
-            .to(mvTitles,{autoAlpha:1, duration:1.3, ease:'power1.in'},'-=.5')
-            .to('.js-top-mv__button',{autoAlpha:1, duration:1.3, ease:'power1.in'},'<')
-        });
-        // 768px以上の時
-        mm.add('(min-width:768px)',function(){
-            openingTL
-            .to(mvTitles,{delay:1, autoAlpha:0, duration:1 ,ease:'power4.in', color: '#fff'})
-            .to(turtleImages,{delay:.5, y:0,duration:2.5 ,ease:'power4.out', stagger:{each:.2}},'-=.5')
-            .to('.js-header',{y:0, duration:1},'-=1.3')
-            .to(mvTitles,{autoAlpha:1, duration:2.5, ease:'power4.in'},'-=2.5')
-            .to('.js-top-mv__button',{autoAlpha:1, duration:2.5, ease:'power4.in'},'-=2.2')
-        });
+    let openingTL = gsap.timeline();
+    let turtleImages = document.querySelectorAll('.js-top-mv__animation-image > *');
+    let mvTitles = document.querySelectorAll('.js-top-mv__title > *');
+    // 767px以下の時
+    mm.add('(max-width:767px)',function(){
+        openingTL
+        .to('.js-header',{delay:.5,y:0, duration:1})
+        .to(mvTitles,{autoAlpha:1, duration:1.3, ease:'power1.in'},'-=.7')
+        .to('body',{overflow:'visible'})
+    });
+    // 768px以上の時
+    mm.add('(min-width:768px)',function(){
+        openingTL
+        .to(mvTitles,{delay:.5, autoAlpha:0, duration:1 ,ease:'power4.in', color: '#fff'})
+        .to(turtleImages,{delay:.5, y:0,duration:2.5 ,ease:'power4.out', stagger:{each:.2}},'-=1')
+        .to('.js-header',{y:0, duration:1},'-=1.3')
+        .to(mvTitles,{autoAlpha:1, duration:2, ease:'power4.in'},'-=1.5')
+        .to('body',{overflow:'visible'})
+    });
 
-
-        // 画像アニメーション
-        mm.add('(max-width:767px)',function(){
-            gsap.utils.toArray('.js-image').forEach(Image =>{
-                let img = Image.querySelector('img');
-                let span = Image.querySelector('span');
-                let TL = gsap.timeline({scrollTrigger:{
-                    trigger: Image,
-                    start: 'top 100%',
-                    toggleActions:'play none none reverse',
-                }})
-                TL
-                .to(img, {delay:.5, duration:.5, x:0, ease:'power:4'})
-                .to(span,{duration:.5, x:0, ease:'power:4'},'<=')
-                .to(span, {delay:.2, duration:.5, x:'-101%', ease:'power:4'})
-            });
+    // 画像アニメーション
+    mm.add('(max-width:767px)',function(){
+        gsap.utils.toArray('.js-image').forEach(Image =>{
+            let img = Image.querySelector('img');
+            let span = Image.querySelector('span');
+            let TL = gsap.timeline({scrollTrigger:{
+                trigger: Image,
+                start: 'top 100%',
+                toggleActions:'play none none reverse',
+            }})
+            TL
+            .to(img, {delay:.5, duration:.5, x:0, ease:'power:4'})
+            .to(span, {duration:.5, x:0, ease:'power:4'},'<=')
+            .to(span, {delay:.2, duration:.5, x:'-101%', ease:'power:4'})
         });
-        mm.add('(min-width:768px)',function(){
-            gsap.utils.toArray('.js-image').forEach(Image =>{
-                let img = Image.querySelector('img');
-                let source = Image.querySelector('source');
-                let span = Image.querySelector('span');
-                let TL = gsap.timeline({scrollTrigger:{
-                    trigger: Image,
-                    start: 'top 100%',
-                    toggleActions:'play none none reverse',
-                }})
-                TL
-                .to(img, {delay:.5, duration:.5, x:0, ease:'power:4'})
-                .to(source, {duration:.5, x:0, ease:'power:4'},'<=')
-                .to(span,{duration:.5, x:0, ease:'power:4'},'<=')
-                .to(span, {delay:.2, duration:.5, x:'-101%', ease:'power:4'})
-            });
-        })
-
+    });
+    mm.add('(min-width:768px)',function(){
+        gsap.utils.toArray('.js-image').forEach(Image =>{
+            let img = Image.querySelector('img');
+            let source = Image.querySelector('source');
+            let span = Image.querySelector('span');
+            let TL = gsap.timeline({scrollTrigger:{
+                trigger: Image,
+                start: 'top 100%',
+                toggleActions:'play none none reverse',
+            }})
+            TL
+            .to(img, {delay:.5, duration:.5, x:0, ease:'power:4'})
+            .to(source, {duration:.5, x:0, ease:'power:4'},'<=')
+            .to(span,{duration:.5, x:0, ease:'power:4'},'<=')
+            .to(span, {delay:.2, duration:.5, x:'-101%', ease:'power:4'})
+        });
     })
-
+})
 
