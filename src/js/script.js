10d9ef4be0f98bc73a416b4af155db10ab8ccefa
{morimasashi} <{m.m.19910905@gmail.com}>
2023-08-11 19:45:44 +0900

    230811_1945

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,26 @@
 
-document.addEventListener('DOMContentLoaded',function(){
+// クッキー登録
+function setCookie(name, value, days) {
+    const date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    const expires = "expires=" + date.toUTCString();
+    document.cookie = name + "=" + value + ";" + expires + ";path=/";
+}
+
+// クッキーを取得
+function getCookie(name) {
+    const value = "; " + document.cookie;// 全てのクッキーの文字列を取得し、先頭に"; "を追加
+    const parts = value.split("; " + name + "=");// クッキーの文字列を分割し、指定された名前の前にある部分と後ろにある部分を配列に格納
+    if (parts.length === 2){
+        return parts.pop().split(";").shift();// 名前が見つかった場合、その値を取得し返します
+    }else{
+        return "";// 名前が見つからなかった場合、空の文字列を返します
+    }
+}
+
+// 初回ローディング
+function playAnimation(){
     const mm = gsap.matchMedia();
-// opening
     let openingTL = gsap.timeline();
     let turtleImages = document.querySelectorAll('.js-top-mv__animation-image > *');
     let mvTitles = document.querySelectorAll('.js-top-mv__title > *');
@@ -14,11 +33,11 @@ document.addEventListener('DOMContentLoaded',function(){
         .to(mvTitles,{autoAlpha:1, duration:1.3, ease:'power1.in'},'-=.7')
         .add(() => {
             body.classList.remove('is-open');
+            setCookie("animationPlayed", "true", 1/24);//1時間
         });
     });
     // 768px以上の時
     mm.add('(min-width:768px)',function(){
-        // body.classList.add('is-open');
         openingTL
         .to(mvTitles,{delay:.5, autoAlpha:0, duration:1 ,ease:'power4.in', color: '#fff'})
         .to(turtleImages,{delay:.5, y:0,duration:2.5 ,ease:'power4.out', stagger:{each:.2}},'-=1')
@@ -26,10 +45,33 @@ document.addEventListener('DOMContentLoaded',function(){
         .to(mvTitles,{autoAlpha:1, duration:2, ease:'power4.in'},'-=1.5')
         .add(() => {
             body.classList.remove('is-open');
+            setCookie("animationPlayed", "true", 1/24);//1時間
         });
     });
+}
 
-    // 画像アニメーション
+// 2回目以降のリロード
+function hideAnimation(){
+    let turtleImages = document.querySelectorAll('.js-top-mv__animation-image > *');
+    let mvTitles = document.querySelectorAll('.js-top-mv__title > *');
+    gsap.set('.js-header',{y:0})
+    gsap.set(mvTitles,{autoAlpha:1, color:'#fff'})
+    gsap.set(turtleImages,{y:0})
+}
+
+// クッキー判定
+document.addEventListener("DOMContentLoaded", function() {
+    const animationPlayed = getCookie("animationPlayed");
+    if (animationPlayed) {
+        hideAnimation();
+    } else {
+        playAnimation();
+    }
+});
+
+// 画像アニメーション
+document.addEventListener("DOMContentLoaded", function() {
+    const mm = gsap.matchMedia();
     mm.add('(max-width:767px)',function(){
         gsap.utils.toArray('.js-image').forEach(Image =>{
             let img = Image.querySelector('img');
@@ -62,13 +104,15 @@ document.addEventListener('DOMContentLoaded',function(){
             .to(span, {delay:.2, duration:.5, x:'-101%', ease:'power:4'})
         });
     })
-})
+});
+
+
 
 
 
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
 
-// ハンバーガー、ドロワー
+    // ハンバーガー、ドロワー
     $(".js-hamburger, .js-drawer, .sp-nav__items > a").click(function () {
         $(".js-hamburger").toggleClass("is-open");
         $(".header").toggleClass("is-open");
@@ -76,32 +120,32 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
         $("body").toggleClass("is-open");
     });
 
-// スライダー
-// js-top-mv-sp-swiper
+    // スライダー
+    // mv-sp-swiper
     let topMvSpSwipeOption = {
         loop: true,
         effect: 'fade',
-        // autoplay: {
-        //     delay: 3000,
-        //     disableOnInteraction: false,
-        // },
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+        },
         speed: 1200,
     }
     const swiper = new Swiper(".js-top-mv__sp-swiper", topMvSpSwipeOption)
 
-// js-top-mv-pc-swiper
+    // mv-pc-swiper
     let topMvPcSwipeOption = {
         loop: true,
         effect: 'fade',
-        // autoplay: {
-        //     delay: 4000,
-        //     disableOnInteraction: false,
-        // },
+        autoplay: {
+            delay: 4000,
+            disableOnInteraction: false,
+        },
         speed: 1500,
     }
     new Swiper(".js-top-mv__pc-swiper", topMvPcSwipeOption)
 
-// js-top-campaign__swiper
+    // キャンペーンのswiper
     let topCampaignSwipeOption = {
         loop: true,
         autoplay: {
@@ -118,10 +162,10 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
     new Swiper(".js-top-campaign__swiper", topCampaignSwipeOption)
 
 
-// 初期はボタンを非表示に
+    // 初期はボタンを非表示に
     var topButton = $('.page-top');
     topButton.hide();
-// スクロールするとボタンを表示
+    // スクロールするとボタンを表示
     $(window).scroll(function () {
         if ($(this).scrollTop() > 100) {
         topButton.fadeIn();
@@ -130,7 +174,7 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
         }
     });
 
-// page-topボタンをフッターに被らないように
+    // page-topボタンをフッターに被らないように
     var footer = document.querySelector(".footer");
     window.addEventListener("scroll", function() {
         var visibleFooterHeight = window.innerHeight - footer.getBoundingClientRect().top;
